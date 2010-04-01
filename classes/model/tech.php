41a25c84d35b410f892ec2c605125093b4bc9782
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-04-01 18:07:03 +0000

    Hack to get a tech's client relation.

--- classes/model/tech.php
+++ classes/model/tech.php
@@ -9,4 +9,24 @@ class Model_Tech extends ORM
         'job_tickets' => array('foreign_key' => 'assigned_tech_id'),
         'tech_group_levels' => array('through' => 'tech_group_level_tech')
     );
+
+    public function __get($column)
+    {
+        // client relation hack, since client_id does not map to the correct client
+        if ($column == 'client')  {
+            $this->_load();
+
+            if (isset($this->_related[$column])) {
+                $model = $this->_related[$column];
+            } else {
+                $model = ORM::factory($column);
+            }
+
+            $model->where('client.user_name', '=', $this->_object['user_name'])->find();
+
+            return $this->_related[$column] = $model;
+        }
+
+        return parent::__get($column);
+    }
 }
