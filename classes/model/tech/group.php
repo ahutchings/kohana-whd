2e7a0531878b353e3753d8f4f2ed7fcb4c48fd1c
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-04-14 21:08:54 +0000

    Add a groups feed.

--- classes/model/tech/group.php
+++ classes/model/tech/group.php
@@ -5,4 +5,8 @@ class Model_Tech_Group extends ORM
     protected $_primary_key = 'id';
     protected $_primary_val = 'name';
     protected $_table_names_plural = FALSE;
+
+    protected $_has_many = array(
+        'tech_group_levels' => array()
+    );
 }
