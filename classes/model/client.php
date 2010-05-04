c65e9b5d821d2cf9966f6079573dff105073ed30
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-05-04 16:17:04 +0000

    Add client -> tech relation hack.

--- classes/model/client.php
+++ classes/model/client.php
@@ -4,4 +4,24 @@ class Model_Client extends ORM
 {
     protected $_primary_key = 'client_id';
     protected $_table_names_plural = FALSE;
+
+    public function __get($column)
+    {
+        // tech relation hack
+        if ($column == 'tech')  {
+            $this->_load();
+
+            if (isset($this->_related[$column])) {
+                $model = $this->_related[$column];
+            } else {
+                $model = ORM::factory($column);
+            }
+
+            $model->where('tech.user_name', '=', $this->_object['user_name'])->find();
+
+            return $this->_related[$column] = $model;
+        }
+
+        return parent::__get($column);
+    }
 }
