4d97c54b3ed939810d1d1570930cb1d397ebb11e
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-06-07 13:46:58 +0000

    Add tech relation to message model.

--- classes/kohana/model/message.php
+++ classes/kohana/model/message.php
@@ -4,4 +4,8 @@ class Kohana_Model_Message extends ORM
 {
     protected $_primary_key = 'id';
     protected $_table_names_plural = FALSE;
+
+    protected $_belongs_to = array(
+        'tech' => array(),
+    );
 }
